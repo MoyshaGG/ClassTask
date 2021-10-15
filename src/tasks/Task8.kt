13a43f6d9e3494e53b99c730c555f07f0c87131c
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2021-10-15 21:39:55 +0300

    Third 2.0

--- src/tasks/Task8.kt
+++ src/tasks/Task8.kt
@@ -1,4 +1,60 @@
 package tasks
 
-class Task8 {
+import javax.xml.stream.Location
+fun main()
+{
+    val husky:Dog = Dog("Husky","meet","siberia")
+    val sphinx:Cat = Cat("Normal Meow","fish","flat")
+    val plotva:Horse = Horse("1л.с.","apples","paddock")
+
+    val vetDoctor:Veterinar = Veterinar()
+    val animals: Array<Animal> = arrayOf(husky,sphinx,plotva)
+    val animal = animals
+
+    for(animal in animals)
+    {
+        vetDoctor.treatAnimal(animal)
+    }
 }
+open class Animal(var food:String, var location:String)
+{
+    open fun makeNoise()
+     {
+         println("Такое-то животное спит")
+     }
+}
+class Dog(var breed:String, food:String,
+ location: String):Animal(food, location)
+{
+    override fun makeNoise()
+    {
+        println("Собака спить")
+    }
+}
+class Cat(var meow:String, food: String,
+location: String):Animal(food,location)
+{
+    override fun makeNoise()
+    {
+        println("Кіт спить")
+    }
+}
+class Horse(var HorsePower:String, food: String,
+          location: String): Animal(food,location)
+{
+    override fun makeNoise()
+    {
+        println("Кінь спить")
+    }
+}
+class Veterinar()
+{
+    fun treatAnimal(animal:Animal)
+    {
+        println("Їжа:${animal.food}, Локація:${animal.location}")
+
+    }
+
+
+}
+
